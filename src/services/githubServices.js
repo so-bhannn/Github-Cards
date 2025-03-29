2d947067577a4efa4a8e9058dca57016cc2a2ac5
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-03-29 13:59:56 +0530

    create getRepoLanguages to fetch languages of a repo

--- src/services/githubServices.js
+++ src/services/githubServices.js
@@ -45,3 +45,19 @@ const getUserRepos = async(username)=>{
       return null
     }
 }
+
+const getRepoLanguages = async(username, repo)=>{
+    try{
+        const headers = GITHUB_TOKEN ? { 
+            'Authorization': `token ${GITHUB_TOKEN}`,
+            'Accept': 'application/vnd.github.v3+json'
+          } : {};
+        const response=await fetch(`https://api.github.com/repos/${username}/${repo}/languages`,{headers})
+        const data = await response.json()
+        return data
+    }
+    catch(error){
+        console.error(`Error fetching languages for the repo: ${repo}`)
+        return null
+    }
+}
